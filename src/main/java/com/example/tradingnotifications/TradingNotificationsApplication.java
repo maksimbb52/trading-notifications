e65d5b00dec76af039af647bfcf413d1eacb7719
maksimbaranov <maksim.bb52@gmail.com>
2025-01-28 21:59:22 +0300

    Initial commit

---
+++ src/main/java/com/example/tradingnotifications/TradingNotificationsApplication.java
@@ -0,0 +1,12 @@
+package com.example.tradingnotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingNotificationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingNotificationsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/tradingnotifications/TradingNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tradingnotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
